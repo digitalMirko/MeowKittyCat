a61f50dfc71cd8fd9df2ae463122d4b9e4690b16
Mirko Cukich <mirkocukich@gmail.com>
2018-10-28 17:49:10 -0500

    Initial Commit

---
+++ Meow Kitty Cat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meow Kitty Cat
+//
+//  Created by Mirko Cukich on 10/28/18.
+//  Copyright © 2018 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
